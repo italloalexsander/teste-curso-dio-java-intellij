ed653a7494b41a9d920628cc6f71729d97a62dc9
Itallo <italloalexsander@gmail.com>
2022-04-27 21:19:19 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,5 +9,43 @@ public class PrimeiroPrograma {
 //        System.out.println("Hello World " + (a + b));
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O problema dos 3 corpos", 308);
+        System.out.println(livro1);
+    }
+
+}
+
+class Livro{
+    private String nome;
+    private Integer numpg;
+
+    public Livro(String nome, Integer numpg) {
+        this.nome = nome;
+        this.numpg = numpg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpg() {
+        return numpg;
+    }
+
+    public void setNumpg(Integer numpg) {
+        this.numpg = numpg;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpg=" + numpg +
+                '}';
     }
 }
